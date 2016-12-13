5751512100fe8cbceeaad546f1c18fb0efcf9e2e
ingthorb <ingthorb@gmail.com>
2016-12-13 13:20:01 +0000

    Adds updated xamarin forms package

--- XFMovieSearch/XFMovieSearch/MovieListXF.xaml.cs
+++ XFMovieSearch/XFMovieSearch/MovieListXF.xaml.cs
@@ -14,5 +14,14 @@ namespace XFMovieSearch
         {
             InitializeComponent();
         }
+        private void Listview_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+
+            this.DisplayAlert(e.SelectedItem.ToString(), string.Empty, "Ok");
+        }
     }
 }
